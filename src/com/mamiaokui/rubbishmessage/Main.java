dd33bebbd9ca01352e0be99548797f077759145f
mamk <mamk@ucweb.com>
2014-07-16 21:38:23 +0800

    send first message

--- src/com/mamiaokui/rubbishmessage/Main.java
+++ src/com/mamiaokui/rubbishmessage/Main.java
@@ -8,12 +8,22 @@ import android.widget.FrameLayout;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
 
 public class Main extends Activity
 {
     /** Called when the activity is first created. */
     FrameLayout mView = null;
-    
+    WebView mWebView = null;
+
+    public Handler mHandler = new Handler() {
+            public void handleMessage (Message msg) {
+                Log.e("mamk", "inject last");
+                mWebView.loadUrl("javascript:var ele = document.getElementById(\"sendMobileCode\"); var ele1 = ele.getElementsByTagName(\"span\")[0];ele1.click();");
+            }
+        };;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -21,17 +31,20 @@ public class Main extends Activity
         setContentView(R.layout.main);
         mView = (FrameLayout)findViewById(R.id.layout);
         mView.setBackgroundColor(Color.RED);
-        WebView mWebView = new WebView(this);
+        mWebView = new WebView(this);
         mView.addView(mWebView);
-        mWebView.loadUrl("http://www.baidu.com/");
+        mWebView.loadUrl("http://my.tv.sohu.com/user/reg/reginfo.do?bru=http%3A//my.tv.sohu.com/");
         mWebView.getSettings().setJavaScriptEnabled(true);
         
         mWebView.setWebViewClient(new WebViewClient() {
                 public void onPageFinished(WebView view, String url) {
-                    view.loadUrl("javascript:document.write(1)");
+                    view.loadUrl("javascript:var ele = document.getElementById(\"regByPhone\"); ele.click(); ");
+                    view.loadUrl("javascript:var ele = document.getElementById(\"mobileTxt\"); ele.setRangeText(\"13602433062\"); ");
+                    mHandler.sendEmptyMessageDelayed(0, 3000);
                 }
             }
             );
 
     }
+
 }
